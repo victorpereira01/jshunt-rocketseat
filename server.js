d53e853a933bc745d234d0c77272afbae75e1fbd
Victor Pereira <victor.cordeiro2@gmail.com>
2020-07-16 21:41:15 -0300

    Get products

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Iniciando o app
+const app = express();
+
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+    { useNewUrlParser: true }
+);
+
+requireDir("./src/models");
+
+//Rotas
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
